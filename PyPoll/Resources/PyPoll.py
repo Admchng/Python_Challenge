fd663881823cbd61ba52875ad77bee28d5811372
Adam <118997959+Admchng@users.noreply.github.com>
2023-01-18 23:21:46 -0800

    Add files via upload

---
+++ PyPoll/Resources/PyPoll.py
@@ -0,0 +1,15 @@
+import csv
+import os
+file_to_load = os.path.join("resources", "election_results.csv")
+total_votes = 0
+candidate_votes = 0
+percentage_won = 0
+candidates = 0
+candidates_won = ""
+total_won = 0
+
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+    print(type(file_reader))
+    headers = next(file_reader)
+    print(headers)
