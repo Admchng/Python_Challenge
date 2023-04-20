63e13c1642a04c12a30b72fdc5f5a2f04526fe94
Adam <118997959+Admchng@users.noreply.github.com>
2023-04-20 14:02:09 -0700

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -1,7 +1,12 @@
+#import modules
 import csv
 import os
+
+#sets the file path
 file_to_load = os.path.join("resources", "election_results.csv")
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+
 total_votes = 0
 candidate_votes = 0
 percentage_won = 0
@@ -9,6 +14,7 @@ candidates = 0
 candidates_won = ""
 total_won = 0
 
+#read the csv file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     print(type(file_reader))
